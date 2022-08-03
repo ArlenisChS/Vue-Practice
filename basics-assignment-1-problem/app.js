6d9ee1cbc49288092196870d0dfbda5703ac7422
Arlenis Ching Suarez <42244318+ArlenisChS@users.noreply.github.com>
2022-08-03 10:22:23 +0200

    First Assignment

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,10 @@
+const newapp = Vue.createApp({
+    data() {
+        return {
+            name: 'Arlenis',
+            age: 25,
+            image_url: 'https://th.bing.com/th/id/OIP.FBnR9Hk0muCwQp2sUxNEjgHaFj?pid=ImgDet&rs=1'
+        }   
+    }
+});
+newapp.mount('#assignment');
